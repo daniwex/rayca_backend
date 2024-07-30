8098b7ef67727bf28f024cea0ee4a13cdc6664e8
adenikinju daniel <adenikinjudaniel@gmail.com>
2024-07-30 07:17:19 -0400

    made changes to packages swagger and controller

--- controllers/login.js
+++ controllers/login.js
@@ -15,7 +15,7 @@ const login = async (req, res) => {
     return res.status(404).send("invalid credentials");
   }
   const token = user.generateJWT();
-  res.status(200).json("success");
+  res.status(200).json({user:user.email, token, message:"success"});
 };
 
 const register = async (req, res) => {
