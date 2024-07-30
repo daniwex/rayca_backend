dd68231b65c9f92a4feeea589a1b8f4b2c59ba18
adenikinju daniel <adenikinjudaniel@gmail.com>
2024-07-30 01:45:30 -0400

    made changes to index and vercel

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-const authRouter = require('./routes/ticket')
-const loginAuthRouter = require('./routes/auth')
-const connectMongoose = require('./db/connectDB')
-const express = require('express')
-const auth = require('./middleware/auth')
-const createRouter = require('./routes/createusers')
-const isAdmin = require('./middleware/admin')
-const cors = require('cors');
-const xss = require('xss-clean')
-const yaml = require('yamljs');
+const authRouter = require("./routes/ticket");
+const loginAuthRouter = require("./routes/auth");
+const connectMongoose = require("./db/connectDB");
+const express = require("express");
+const auth = require("./middleware/auth");
+const createRouter = require("./routes/createusers");
+const isAdmin = require("./middleware/admin");
+const cors = require("cors");
+const xss = require("xss-clean");
+const yaml = require("yamljs");
 let PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-const swagger = yaml.load('swagger.yaml');
-const swaggerUI = require('swagger-ui-express');
+const swagger = yaml.load("swagger.yaml");
+const swaggerUI = require("swagger-ui-express");
 
-app.use(express.static('public'))
-
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-app.use(xss())
-app.use('/api/tickets/',auth,authRouter)
-app.use('/api/',loginAuthRouter)
-app.use('/api/',auth,isAdmin,createRouter)
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swagger));
+app.use(xss());
 
+app.use("/api/tickets/", auth, authRouter);
+app.use("/api/", loginAuthRouter);
+app.use("/api/", auth, isAdmin, createRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swagger));
 
-app.listen(PORT, () => {
-     connectMongoose()
-})
+const start = async () => {
+  try {
+    await connectMongoose(process.env.MONGO_URI);
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
- 
+start();
